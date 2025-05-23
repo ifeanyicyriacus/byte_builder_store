92c50662f96d2189488888dd8040be90ea79f5dd
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-05-23 15:42:07 +0100

    progress

---
+++ estore/src/main/java/org/estore/estore/controller/OrderController.java
@@ -0,0 +1,25 @@
+package org.estore.estore.controller;
+
+import lombok.AllArgsConstructor;
+import org.estore.estore.dto.request.CreateOrderRequest;
+import org.estore.estore.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/order")
+@AllArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<?> createOrder(@RequestBody CreateOrderRequest createOrderRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(orderService.create(createOrderRequest));
+    }
+
+}
